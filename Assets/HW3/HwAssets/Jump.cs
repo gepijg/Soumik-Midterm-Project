66cee151efaf0597a2144e02a32289f4f8df38b5
gepijg <154713775+gepijg@users.noreply.github.com>
2025-10-29 14:32:52 -0400

    Stuff 4

--- Assets/HW3/HwAssets/Jump.cs
+++ Assets/HW3/HwAssets/Jump.cs
@@ -5,6 +5,7 @@ public class Jump : MonoBehaviour
 public float speed = 0.01f;
     public float jumpSpeed = 10;
     public Rigidbody2D rb;
+    public bool canJump = true;
     Vector2 velocity;
     float gravity;
     float friction;
@@ -20,5 +21,13 @@ public float speed = 0.01f;
          if(Input.GetKey(KeyCode.Space)) {
               rb.linearVelocity = new Vector2(0, jumpSpeed);
          }
+
     }
+   // void OnCollisionEnter2D(Collision2D collision)
+   // {
+     //   if (collision.gameObject.CompareTag("Ground"))
+      //  {
+        //    canJump = true;
+       // }
+   // }
 }
